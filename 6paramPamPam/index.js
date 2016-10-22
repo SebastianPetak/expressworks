0f3d725560516b389b5cb4514552e4b7a784d04e
SebastianPetak <sebastian.petak@gmail.com>
2016-10-22 08:48:15 -0500

    Added exercise 6 and 7

--- 6paramPamPam/index.js
+++ 6paramPamPam/index.js
@@ -1,10 +1,21 @@
+var express = require('express');
+var app = express();
+var crypto = require('crypto');
+
 var http = require('http');
 
-var server = createServer(function(req, res) {
-	req.param('id', function(req,res,next,id) {
-		req.id = id
-	})
-})
+function createCrypt(id) {
+    return crypto.createHash('sha1')
+    .update(new Date().toDateString() + id)
+    .digest('hex');
+}
+
+
+app.put('/message/:id', function(req, res) {
+    var id = req.params.id;
+    res.send(createCrypt(id));
+    console.log(createCrypt(id));
+}).listen(process.argv[2]);
 
 // app.param
 // require('crypto') SHA-1 and hash
